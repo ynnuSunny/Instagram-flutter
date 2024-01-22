ef4940ed4421316f4ffe17c515067c0d51325182
ynnuSunny <sunnysutradhar38@gmail.com>
2024-01-22 12:43:44 +0600

    update responsive layout

--- lib/responsive/responsive_layout.dart
+++ lib/responsive/responsive_layout.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import '../providers/user_provider.dart';
 import '../utils/global_variable.dart';
+import 'package:provider/provider.dart';
 
-class ResponsiveLayout extends StatelessWidget {
+class ResponsiveLayout extends StatefulWidget {
   final Widget mobileScreenLayout;
   final Widget webScreenLayout;
   const ResponsiveLayout({
@@ -11,14 +13,30 @@ class ResponsiveLayout extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<ResponsiveLayout> createState() => _ResponsiveLayoutState();
+}
+
+class _ResponsiveLayoutState extends State<ResponsiveLayout> {
+  @override
+  void initState() {
+    super.initState();
+    addData();
+  }
+
+  addData() async {
+    UserProvider userProvider =
+        Provider.of<UserProvider>(context, listen: false);
+    await userProvider.refreshUser();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
       if (constraints.maxWidth > webScreenSize) {
-       return webScreenLayout;
+        // 600 can be changed to 900 if you want to display tablet screen with mobile screen layout
+        return widget.webScreenLayout;
       }
-      return mobileScreenLayout;
+      return widget.mobileScreenLayout;
     });
   }
 }
-
-
